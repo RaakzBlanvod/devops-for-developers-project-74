f719e38fd5d0f28d6a55c0b80348b8266f0d2398
RaakzBlanvod <icky6119@gmail.com>
2026-03-26 18:12:44 +0300

    Update config.cjs

--- app/config/config.cjs
+++ app/config/config.cjs
@@ -7,18 +7,18 @@ module.exports = {
   },
   production: {
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME || 'blog_development',
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'postgres',
-    port: process.env.DATABASE_PORT || 5432,
-    host: process.env.DATABASE_HOST || 'db',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
   },
   test: {
     dialect: 'postgres',
-    database: process.env.DATABASE_NAME || 'blog_development',
-    username: process.env.DATABASE_USERNAME || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'password',  // ← здесь password для тестов
-    port: process.env.DATABASE_PORT || 5432,
-    host: process.env.DATABASE_HOST || 'db',
+    database: process.env.DATABASE_NAME,
+    username: process.env.DATABASE_USERNAME,
+    password: 'password',  // ← жёстко прописываем password для тестов
+    port: process.env.DATABASE_PORT,
+    host: process.env.DATABASE_HOST,
   },
 };
